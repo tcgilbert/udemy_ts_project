df54ee2852d87d028b159bc48e14f005ac5db0f5
Thomas Gilbert <tcgilbert94@gmail.com>
2021-03-26 10:59:14 -0400

    [index] now communicating users code by posting messages to the iframe context

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
     const ref = useRef<any>();
+    const iframe = useRef<any>();
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -34,13 +35,27 @@ const App = () => {
             },
         });
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(
+            result.outputFiles[0].text,
+            "*"
+        );
     };
 
     const html = `
-        <script>
-        ${code}
-        </script>
+        <html>
+         <head>
+         </head>
+         <body>
+            <div id="root">
+            </div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    eval(event.data);
+                }, false);
+            </script>
+         </body>
+        </html>
     `;
 
     return (
@@ -53,7 +68,7 @@ const App = () => {
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
-            <iframe sandbox="allow-scripts" srcDoc={html} />
+            <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };
