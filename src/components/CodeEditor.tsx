a8b747fd87242c601ab4e95c11adf2cc734a30f1
Thomas Gilbert <tcgilbert94@gmail.com>
2021-03-29 16:41:27 -0400

    [CodeEditor] more monaco editor config

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <MonacoEditor height="500px" theme="dark" language="javascript" />;
+    return (
+        <MonacoEditor
+            height="500px"
+            theme="dark"
+            language="javascript"
+            options={{
+                wordWrap: "on",
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+                automaticLayout: true,
+            }}
+        />
+    );
 };
 
 export default CodeEditor;
