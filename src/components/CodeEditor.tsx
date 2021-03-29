50b36cb0df732d61d117bd0881aab1dddec8cc10
Thomas Gilbert <tcgilbert94@gmail.com>
2021-03-29 16:20:53 -0400

    [CodeEditor] adds editor configurations

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <MonacoEditor />;
+    return <MonacoEditor height="500px" theme="dark" language="javascript" />;
 };
 
 export default CodeEditor;
